e752e487bb0b7746217e02026586793711809ef0
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2022-12-16 20:14:25 +0200

    Adding Button Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -25,9 +25,28 @@ ThemeData getApplicationTheme() {
           elevation: AppSize.s4,
           shadowColor: ColorManager.lightPrimary,
           titleTextStyle:
-              getRegularStyle(fontSize: FontSize.s16, color: Colors.white))
-      //button theme
+              getRegularStyle(fontSize: FontSize.s16, color: Colors.white)),
+      // button theme
+      buttonTheme: ButtonThemeData(
+        shape: const StadiumBorder(),
+        disabledColor: ColorManager.grey1,
+        buttonColor: ColorManager.primary,
+        splashColor: ColorManager.lightPrimary
+      ),
 
-      //input decoration theme (text form field)
+      // elevated button theme
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          textStyle: getRegularStyle(color: ColorManager.white, fontSize: FontSize.s17),
+          backgroundColor: ColorManager.primary,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(AppSize.s12)
+          ) 
+        )
+      ),
+      // text theme
+
+      
+      // input decoration theme (text form field)
       );
 }
