56ff4e4c6300a7584203fe64b59c2a91d5876d41
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2022-12-16 19:54:38 +0200

    Adding App Bar Theme

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_app/presentation/color_manager.dart';
+import 'package:shopping_app/presentation/font_manager.dart';
+import 'package:shopping_app/presentation/styles_manager.dart';
 import 'package:shopping_app/presentation/values_manager.dart';
 
-ThemeData getApplicationTheme(){
+ThemeData getApplicationTheme() {
   return ThemeData(
-    // main colors
-    primaryColor: ColorManager.primary,
-    primaryColorLight: ColorManager.lightPrimary,
-    primaryColorDark: ColorManager.darkPrimary,
-    disabledColor: ColorManager.grey1,
-    // cardview theme
-    cardTheme: CardTheme(
-      color: ColorManager.white,
-      shadowColor: ColorManager.grey1,
-      elevation: AppSize.s4
-    )
-    // app bar theme
+      // main colors
+      primaryColor: ColorManager.primary,
+      primaryColorLight: ColorManager.lightPrimary,
+      primaryColorDark: ColorManager.darkPrimary,
+      disabledColor: ColorManager.grey1,
+      splashColor: ColorManager.lightPrimary,
+      // cardview theme
+      cardTheme: CardTheme(
+        color: ColorManager.white,
+        shadowColor: ColorManager.grey1,
+        elevation: AppSize.s4,
+      ),
+      // app bar theme
+      appBarTheme: AppBarTheme(
+          centerTitle: true,
+          color: ColorManager.primary,
+          elevation: AppSize.s4,
+          shadowColor: ColorManager.lightPrimary,
+          titleTextStyle:
+              getRegularStyle(fontSize: FontSize.s16, color: Colors.white))
+      //button theme
 
-    //button theme
-
-    //input decoration theme (text form field)
-  );
+      //input decoration theme (text form field)
+      );
 }
