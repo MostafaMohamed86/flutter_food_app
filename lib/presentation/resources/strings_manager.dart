7a87178814ecee6c4f750e0b86d01ef972366728
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2022-12-17 20:18:52 +0200

    Adding Assets for Onboarding Screen

--- lib/presentation/resources/strings_manager.dart
+++ lib/presentation/resources/strings_manager.dart
@@ -1,3 +1,14 @@
 class AppStrings{
-  static const noRouteFound = "No Route Found";
+  static const noRouteFound = "No Page Found";
+  static const onBoardingTitle1 = "The Best Food1";
+  static const onBoardingTitle2 = "The Best Food2";
+  static const onBoardingTitle3 = "The Best Food3";
+  static const onBoardingTitle4 = "The Best Food4";
+  static const onBoardingTitle5 = "The Best Food5";
+
+  static const onBoardingSubTitle1 = "The Best Food is made for your taste in food1";
+  static const onBoardingSubTitle2 = "The Best Food is made for your taste in food2";
+  static const onBoardingSubTitle3 = "The Best Food is made for your taste in food3";
+  static const onBoardingSubTitle4 = "The Best Food is made for your taste in food4";
+  static const onBoardingSubTitle5 = "The Best Food is made for your taste in food5";
 }
