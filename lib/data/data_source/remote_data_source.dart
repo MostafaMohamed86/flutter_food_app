87e4568158ec8fce4d11f5a4cdf9be48f840ccbf
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2022-12-25 19:54:37 +0200

    Creating Remote Data Source

---
+++ lib/data/data_source/remote_data_source.dart
@@ -0,0 +1,17 @@
+import 'package:shopping_app/data/network/app_api.dart';
+import 'package:shopping_app/data/network/requests.dart';
+import 'package:shopping_app/data/response/responses.dart';
+
+abstract class RemoteDataSource{
+  Future<AuthenticationResponse>login(LoginRequest loginRequest);
+}
+
+class RemoteDataSourceImpl implements RemoteDataSource{
+  final AppServicesClient _appServicesClient;
+  RemoteDataSourceImpl(this._appServicesClient);
+  @override
+  Future<AuthenticationResponse> login(LoginRequest loginRequest) async{
+    return await _appServicesClient.login(loginRequest.email, loginRequest.password);
+  }
+  
+}
