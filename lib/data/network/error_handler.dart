a36d6120da0a901f67087e846303e1624ffe8d4c
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2022-12-26 17:53:59 +0200

    Adding Extension on Data Sources

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_app/data/network/failure.dart';
+
 enum DataSource{
   SUCCESS,
   NO_CONTENT,
@@ -14,13 +17,47 @@ enum DataSource{
   NO_INTERNET_CONNECTION
 }
 
-class ResoponseCode{
+extension DataSourceExtension on DataSource{
+  Failure getFailure(){
+    switch(this){
+
+      case DataSource.SUCCESS:
+        return Failure(ResponseCode.SUCCESS, ResponseMessage.SUCCESS);
+      case DataSource.NO_CONTENT:
+        return Failure(ResponseCode.NO_CONTENT, ResponseMessage.NO_CONTENT);
+      case DataSource.BAD_REQUEST:
+        return Failure(ResponseCode.BAD_REQUEST, ResponseMessage.BAD_REQUEST);
+      case DataSource.FORBIDDEN:
+        return Failure(ResponseCode.FORBIDDEN, ResponseMessage.FORBIDDEN);
+      case DataSource.UNAUTHORISED:
+        return Failure(ResponseCode.UNAUTHORISED, ResponseMessage.UNAUTHORISED);
+      case DataSource.NOT_FOUND:
+        return Failure(ResponseCode.NOT_FOUND, ResponseMessage.NOT_FOUND);
+      case DataSource.INTERNAL_SERVER_ERROR:
+        return Failure(ResponseCode.INTERNAL_SERVER_ERROR, ResponseMessage.INTERNAL_SERVER_ERROR);
+      case DataSource.CONNECT_TIMEOUT:
+        return Failure(ResponseCode.CONNECT_TIMEOUT, ResponseMessage.CONNECT_TIMEOUT);
+      case DataSource.CANCEL:
+        return Failure(ResponseCode.CANCEL, ResponseMessage.CANCEL);
+      case DataSource.RECIEVE_TIMEOUT:
+        return Failure(ResponseCode.RECIEVE_TIMEOUT, ResponseMessage.RECIEVE_TIMEOUT);
+      case DataSource.SEND_TIMEOUT:
+        return Failure(ResponseCode.SEND_TIMEOUT, ResponseMessage.SEND_TIMEOUT);
+      case DataSource.CACHE_ERROR:
+        return Failure(ResponseCode.CACHE_ERROR, ResponseMessage.CACHE_ERROR);
+      case DataSource.NO_INTERNET_CONNECTION:
+        return Failure(ResponseCode.NO_INTERNET_CONNECTION, ResponseMessage.NO_INTERNET_CONNECTION);
+    }
+  }
+}
+
+class ResponseCode{
   static const int SUCCESS = 200; // success with data
   static const int NO_CONTENT = 201; // success with no data (no content)
   static const int BAD_REQUEST = 400; // failure, API rejected request 
   static const int UNAUTHORISED = 401; // failure, user is not authorised  
   static const int FORBIDDEN = 403; // failure, user API rejected request
-  static const int NOT_FOUND = 404;
+  static const int NOT_FOUND = 404; // failure, not found
   static const int INTERNAL_SERVER_ERROR = 500; // failure, crash in server side
 
 
@@ -35,13 +72,13 @@ class ResoponseCode{
   static const int UNKNOWN = -7;
 }
 
-class ResoponseMessage{
+class ResponseMessage{
   static const String SUCCESS = "Success"; // success with data
   static const String NO_CONTENT = "Success"; // success with no data (no content)
   static const String BAD_REQUEST = "Bad request, Try again later"; // failure, API rejected request 
   static const String UNAUTHORISED = "User is unauthorised, Try again later"; // failure, user is not authorised  
   static const String FORBIDDEN = "Forbidden request, Try again later"; // failure, user API rejected request
-  static const String NOT_FOUND = "";
+  static const String NOT_FOUND = "Page not found";
   static const String INTERNAL_SERVER_ERROR = "Something went wrong, Try again later"; // failure, crash in server side
 
 
