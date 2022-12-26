c5173888c84abbc9340dd8ea5e22502fb01e573f
MostafaMohamed86 <mostafa.mohamed0869@gmail.com>
2022-12-26 17:38:04 +0200

    Adding Responses Messages

--- lib/data/network/error_handler.dart
+++ lib/data/network/error_handler.dart
@@ -13,3 +13,45 @@ enum DataSource{
   CACHE_ERROR,
   NO_INTERNET_CONNECTION
 }
+
+class ResoponseCode{
+  static const int SUCCESS = 200; // success with data
+  static const int NO_CONTENT = 201; // success with no data (no content)
+  static const int BAD_REQUEST = 400; // failure, API rejected request 
+  static const int UNAUTHORISED = 401; // failure, user is not authorised  
+  static const int FORBIDDEN = 403; // failure, user API rejected request
+  static const int NOT_FOUND = 404;
+  static const int INTERNAL_SERVER_ERROR = 500; // failure, crash in server side
+
+
+
+  //local status code 
+  static const int CONNECT_TIMEOUT = -1;
+  static const int CANCEL = -2;
+  static const int RECIEVE_TIMEOUT = -3;
+  static const int SEND_TIMEOUT = -4;
+  static const int CACHE_ERROR = -5;
+  static const int NO_INTERNET_CONNECTION = -6;
+  static const int UNKNOWN = -7;
+}
+
+class ResoponseMessage{
+  static const String SUCCESS = "Success"; // success with data
+  static const String NO_CONTENT = "Success"; // success with no data (no content)
+  static const String BAD_REQUEST = "Bad request, Try again later"; // failure, API rejected request 
+  static const String UNAUTHORISED = "User is unauthorised, Try again later"; // failure, user is not authorised  
+  static const String FORBIDDEN = "Forbidden request, Try again later"; // failure, user API rejected request
+  static const String NOT_FOUND = "";
+  static const String INTERNAL_SERVER_ERROR = "Something went wrong, Try again later"; // failure, crash in server side
+
+
+
+  //local status code 
+  static const String CONNECT_TIMEOUT = "Time out error, Try again later";
+  static const String CANCEL = "Request was cancelled, Try again later";
+  static const String RECIEVE_TIMEOUT = "Time out error, Try again later";
+  static const String SEND_TIMEOUT = "Time out error, Try again later";
+  static const String CACHE_ERROR = "Cache error, Try again later";
+  static const String NO_INTERNET_CONNECTION = "Please check your internet connection";
+  static const String UNKNOWN = "Something went wrong, try again later";
+}
